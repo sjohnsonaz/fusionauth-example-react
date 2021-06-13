6164d4ebe2ddb0f27d6604d8eaaf38267c3d2ebc
Sean Johnson <sjohnson@sjohnsonaz.com>
2021-06-13 14:19:08 -0700

    Updated pkce.

--- server/helpers/pkce.js
+++ server/helpers/pkce.js
@@ -1,21 +1,32 @@
 const crypto = require('crypto');
 
+/**
+ *
+ * @param {string} str
+ * @returns
+ */
 function base64URLEncode(str) {
-  return str
-    .toString("base64")
-    .replace(/\+/g, "-")
-    .replace(/\//g, "_")
-    .replace(/=/g, "")
+  return str.toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
 }
 
+/**
+ *
+ * @param {crypto.BinaryLike} buffer
+ * @returns
+ */
 function sha256(buffer) {
-  return crypto.createHash("sha256").update(buffer).digest()
+  return crypto.createHash('sha256').update(buffer).digest();
 }
 
 module.exports.generateVerifier = () => {
-  return base64URLEncode(crypto.randomBytes(32))
-}
+  return base64URLEncode(crypto.randomBytes(32));
+};
 
+/**
+ *
+ * @param {crypto.BinaryLike} verifier
+ * @returns
+ */
 module.exports.generateChallenge = (verifier) => {
-  return base64URLEncode(sha256(verifier))
-}
+  return base64URLEncode(sha256(verifier));
+};
